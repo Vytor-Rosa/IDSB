9fb68f4428becff236a5ab2091b2bdb53470953f
K43RU <86034275+Vytor-Rosa@users.noreply.github.com>
2022-10-31 15:02:53 -0300

    Initial commit

---
+++ gedesti/src/main/java/net/weg/gedesti/GedestiApplication.java
@@ -0,0 +1,13 @@
+package net.weg.gedesti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GedestiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GedestiApplication.class, args);
+	}
+
+}
---
+++ gedesti/src/test/java/net/weg/gedesti/GedestiApplicationTests.java
@@ -0,0 +1,13 @@
+package net.weg.gedesti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GedestiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
