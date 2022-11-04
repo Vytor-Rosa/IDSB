87f7ec9cb3271830dce1c71d0104c45f1484ee61
ester <ester_girelli@estudante.sesisenai.org.br>
2022-11-04 16:18:07 -0300

    :+1:

--- gedesti/src/main/java/net/weg/gedesti/dto/AtaDTO.java
+++ gedesti/src/main/java/net/weg/gedesti/dto/AtaDTO.java
@@ -2,14 +2,18 @@ package net.weg.gedesti.dto;
 
 import lombok.Getter;
 import lombok.Setter;
-import net.weg.gedesti.model.entity.Anexo;
 import net.weg.gedesti.model.entity.Pauta;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Getter @Setter
 public class AtaDTO {
-
     private Integer codigoAta;
+    @NotBlank
     private String nomeAta;
+    @NotBlank
     private String problemaAta;
+    @NotNull
     private Pauta pauta;
 }
--- gedesti/src/main/java/net/weg/gedesti/model/entity/Ata.java
+++ gedesti/src/main/java/net/weg/gedesti/model/entity/Ata.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.persistence.*;
-import java.util.List;
 
 @Entity
 @Table(name = "ata")
