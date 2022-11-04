5084b06ef29ce507a88a4ce32906be8108c7d812
K43RU <86034275+Vytor-Rosa@users.noreply.github.com>
2022-11-04 14:11:47 -0300

    ok

--- gedesti/src/main/java/net/weg/gedesti/model/entity/Anexo.java
+++ gedesti/src/main/java/net/weg/gedesti/model/entity/Anexo.java
@@ -1,27 +1,28 @@
-//package net.weg.gedesti.model.entity;
-//
-//import lombok.*;
-//
-//import javax.persistence.*;
-//
-//@Entity
-//@Table(name = "anexo")
-//@NoArgsConstructor
-//@AllArgsConstructor
-//@Getter
-//@Setter
-//public class Anexo {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    @Column(nullable = false, unique = true)
-//
-//    @NonNull
-//    private String nome;
-//
-//    @NonNull
-//    private String tipo;
-//
-//    @Lob
-//    @NonNull
-//    private byte[] dados;
-//}
+package net.weg.gedesti.model.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "anexo")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Anexo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, unique = true)
+    private Integer codigoAnexo;
+
+    @NonNull
+    private String nome;
+
+    @NonNull
+    private String tipo;
+
+    @Lob
+    @NonNull
+    private byte[] dados;
+}
