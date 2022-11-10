759d9042ec44c119fa20156c685d055be969e271
eduardabolgenhagen <bolgenhagene4@gmail.com>
2022-11-10 18:59:20 -0300

    comissao ok

--- gedesti/src/main/java/net/weg/gedesti/controller/PautaController.java
+++ gedesti/src/main/java/net/weg/gedesti/controller/PautaController.java
@@ -2,11 +2,11 @@ package net.weg.gedesti.controller;
 
 import lombok.AllArgsConstructor;
 import net.weg.gedesti.dto.PautaDTO;
-import net.weg.gedesti.model.entity.Comissao;
+//import net.weg.gedesti.model.entity.Comissao;
 import net.weg.gedesti.model.entity.Demanda;
 import net.weg.gedesti.model.entity.Funcionario;
 import net.weg.gedesti.model.entity.Pauta;
-import net.weg.gedesti.model.service.ComissaoService;
+//import net.weg.gedesti.model.service.ComissaoService;
 import net.weg.gedesti.model.service.PautaService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.context.annotation.Bean;
@@ -30,7 +30,7 @@ import java.util.Optional;
 @RequestMapping("/api/pauta")
 public class PautaController {
     private PautaService pautaService;
-    private ComissaoService comissaoService;
+//    private ComissaoService comissaoService;
 
     @GetMapping
     public ResponseEntity<List<Pauta>> findAll() {
@@ -46,13 +46,6 @@ public class PautaController {
     public ResponseEntity<Object> save(@RequestBody @Valid PautaDTO pautaDTO) {
         Pauta pauta = new Pauta();
         BeanUtils.copyProperties(pautaDTO, pauta);
-//        Pauta pautaSalva = pautaService.save(pauta);
-//        List<Comissao> comissao =pautaDTO.getFuncionarios();
-//
-//        for (Comissao comissoes : comissao) {
-//            comissoes.setCodigoPauta(pautaSalva);
-//            comissaoService.save(comissoes);
-//        }
 
         return ResponseEntity.status(HttpStatus.CREATED).body(pautaService.save(pauta));
     }
@@ -65,7 +58,7 @@ public class PautaController {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Erro nenhuma pauta com o codigo: " + codigo);
         }
 
-        List<Comissao> comissaoList = comissaoService.findAll();
+//        List<Comissao> comissaoList = comissaoService.findAll();
         List<Pauta> pautaLista = new ArrayList<>();
         pautaLista.add(pautaOptional.get());
 
