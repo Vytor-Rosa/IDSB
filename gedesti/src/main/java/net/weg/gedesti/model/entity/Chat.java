8957335bb0c774dd76b013a0ca5aa6a9de3f4c91
K43RU <86034275+Vytor-Rosa@users.noreply.github.com>
2022-11-11 14:42:15 -0300

    ok

---
+++ gedesti/src/main/java/net/weg/gedesti/model/entity/Chat.java
@@ -0,0 +1,26 @@
+package net.weg.gedesti.model.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "chat")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Chat {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, unique = true)
+    private Integer codigoChat;
+
+    @OneToOne
+    @JoinColumn(nullable = false)
+    private Demanda demanda;
+
+
+}
---
+++ gedesti/src/main/java/net/weg/gedesti/repository/ChatRepository.java
@@ -0,0 +1,9 @@
+package net.weg.gedesti.repository;
+
+import net.weg.gedesti.model.entity.Chat;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ChatRepository extends JpaRepository<Chat, Integer> {
+}
