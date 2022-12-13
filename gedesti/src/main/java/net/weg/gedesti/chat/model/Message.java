4170b4a09a0d4dbfc5c155f71d3c951af4652c82
K43RU <86034275+Vytor-Rosa@users.noreply.github.com>
2022-12-12 21:48:05 -0300

    ok

---
+++ gedesti/src/main/java/net/weg/gedesti/chat/model/Message.java
@@ -0,0 +1,16 @@
+//package net.weg.gedesti.chat.model;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@Data
+//public class Message {
+//    private String senderName;
+//    private String receiverName;
+//    private String message;
+//    private String date;
+//    private Status status;
+//}
