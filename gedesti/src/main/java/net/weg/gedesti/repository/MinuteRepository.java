a303885a85f576b204e20f95fdbedff3641dd679
Vytor Rosa <86034275+Vytor-Rosa@users.noreply.github.com>
2023-03-28 21:29:56 -0300

    ok

--- gedesti/src/main/java/net/weg/gedesti/repository/MinuteRepository.java
+++ gedesti/src/main/java/net/weg/gedesti/repository/MinuteRepository.java
@@ -4,7 +4,10 @@ import net.weg.gedesti.model.entity.Minute;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 
 @Repository
 public interface MinuteRepository extends JpaRepository <Minute, Integer> {
+    List<Minute> findByMinuteType(String minuteType);
 }
