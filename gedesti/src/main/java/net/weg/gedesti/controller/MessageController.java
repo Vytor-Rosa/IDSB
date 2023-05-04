983339f4585888e8f0608685c52250c960dd98e2
Vytor Rosa <86034275+Vytor-Rosa@users.noreply.github.com>
2023-05-04 15:57:19 -0300

    ok

--- gedesti/src/main/java/net/weg/gedesti/controller/MessageController.java
+++ gedesti/src/main/java/net/weg/gedesti/controller/MessageController.java
@@ -55,16 +55,13 @@ public class MessageController {
     @GetMapping("/{demandCode}")
     public ResponseEntity<?> demandMessages(@PathVariable(value = "demandCode") Integer demandCode) {
         System.out.println("Entrou no get");
-        if(messageService.findAllByDemand(demandCode).size() == 0){
-            return ResponseEntity.status(HttpStatus.OK).body(null);
-        }
         List<Demand> demandList = demandService.findByDemandCode(demandCode);
         for(Demand demand : demandList){
             if(demand.getActiveVersion() == true){
                 return ResponseEntity.ok(messageService.findAllByDemand(demand.getDemandCode()));
             }
         }
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Error! No demand with code: " + demandCode);
+        return ResponseEntity.status(HttpStatus.FOUND).body(demandCode);
     }
 
     @MessageMapping("/demand/{id}") // Mandando a requisição
