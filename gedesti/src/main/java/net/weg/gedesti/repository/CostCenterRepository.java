6451d6e50757c2e8f9dddca202aa0ff4e99a6e1c
leopoglia <92129999+leopoglia@users.noreply.github.com>
2023-05-26 14:16:57 -0300

    ✔

--- gedesti/src/main/java/net/weg/gedesti/repository/CostCenterRepository.java
+++ gedesti/src/main/java/net/weg/gedesti/repository/CostCenterRepository.java
@@ -4,7 +4,10 @@ import net.weg.gedesti.model.entity.CostCenter;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CostCenterRepository extends JpaRepository<CostCenter, Integer> {
 
+    Optional<CostCenter> findByCostCenter(String costCenter);
 }
