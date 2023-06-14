73244d6d45ba90a8b2b691c072ecf8675c9f20ca
leopoglia <92129999+leopoglia@users.noreply.github.com>
2023-06-14 16:51:16 -0300

    ✔

--- gedesti/src/main/java/net/weg/gedesti/DatabaseInitializer.java
+++ gedesti/src/main/java/net/weg/gedesti/DatabaseInitializer.java
@@ -1,13 +1,14 @@
 package net.weg.gedesti;
 
-import net.weg.gedesti.model.entity.Bu;
-import net.weg.gedesti.model.entity.Commission;
-import net.weg.gedesti.model.entity.Worker;
+import net.weg.gedesti.dto.DemandDTO;
+import net.weg.gedesti.model.entity.*;
 import net.weg.gedesti.repository.CommissionRepository;
 import net.weg.gedesti.repository.WorkerRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import net.weg.gedesti.repository.BuRepository;
+
+import java.util.List;
 
 @Component
 public class DatabaseInitializer implements CommandLineRunner {
@@ -24,20 +25,24 @@ public class DatabaseInitializer implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
+
+       addWorkers();
+       addCommissions();
+       addBus();
+    }
+
+    public void addWorkers(){
         Worker requester = new Worker(1, "Leonardo Heitor Poglia", "leonardo@weg.net", "123", "requester", "pt", "machining", false, false, false, false, false, 24);
         Worker analyst = new Worker(2, "Vytor Augusto Rosa", "vytor@weg.net", "123", "analyst", "pt", "ti", false, false, false, false, false, 24);
         Worker ti = new Worker(3, "Ester Girelli", "ester@weg.net", "123", "ti", "pt", "rh", false, false, false, false, false, 24);
         Worker business = new Worker(4, "Eduarda Campos", "eduarda@weg.net", "123", "business", "pt", "ti", false, false, false, false, false, 24);
+        workerRepository.save(requester);
+        workerRepository.save(analyst);
+        workerRepository.save(ti);
+        workerRepository.save(business);
+    }
 
-        Bu bu1 = new Bu(1, "WMO-I – WEG Motores Industrial");
-        Bu bu2 = new Bu(2, "WMO-C – WEG Motores Comercial");
-        Bu bu3 = new Bu(3, "WEN – WEG Energia");
-        Bu bu4 = new Bu(4, "WAU – WEG Automação");
-        Bu bu5 = new Bu(5, "WDS – WEG Digital e Sistemas");
-        Bu bu6 = new Bu(6, "WDC – WEG Drives e Controls");
-        Bu bu7 = new Bu(7, "WTI – WEG Tintas");
-        Bu bu8 = new Bu(8, "WTD – WEG Transmissão e Distribuição");
-
+    public void addCommissions() {
         Commission commission1 = new Commission(1, "CPVM – Comissão de Processos de Vendas e Desenvolvimento de produtos", "CPVM");
         Commission commission2 = new Commission(2, "CPGCI – Comissão de Processos da Cadeia Integrada", "CPGCI");
         Commission commission3 = new Commission(3, "CPGPR – Comissão de Processos de Gestão de Projetos", "CPGPR");
@@ -45,21 +50,6 @@ public class DatabaseInitializer implements CommandLineRunner {
         Commission commission5 = new Commission(5, "CTI – Comitê de TI", "CTI");
         Commission commission6 = new Commission(6, "CWBS – Comitê WEG Business Services", "CWBS");
         Commission commission7 = new Commission(7, "DTI – Diretoria de TI", "DTI");
-
-
-        workerRepository.save(requester);
-        workerRepository.save(analyst);
-        workerRepository.save(ti);
-        workerRepository.save(business);
-
-        buRepository.save(bu1);
-        buRepository.save(bu2);
-        buRepository.save(bu3);
-        buRepository.save(bu4);
-        buRepository.save(bu5);
-        buRepository.save(bu6);
-        buRepository.save(bu7);
-        buRepository.save(bu8);
 
         commissionRepository.save(commission1);
         commissionRepository.save(commission2);
@@ -69,4 +59,24 @@ public class DatabaseInitializer implements CommandLineRunner {
         commissionRepository.save(commission6);
         commissionRepository.save(commission7);
     }
+
+    public void addBus(){
+        Bu bu1 = new Bu(1, "WMO-I – WEG Motores Industrial");
+        Bu bu2 = new Bu(2, "WMO-C – WEG Motores Comercial");
+        Bu bu3 = new Bu(3, "WEN – WEG Energia");
+        Bu bu4 = new Bu(4, "WAU – WEG Automação");
+        Bu bu5 = new Bu(5, "WDS – WEG Digital e Sistemas");
+        Bu bu6 = new Bu(6, "WDC – WEG Drives e Controls");
+        Bu bu7 = new Bu(7, "WTI – WEG Tintas");
+        Bu bu8 = new Bu(8, "WTD – WEG Transmissão e Distribuição");
+        buRepository.save(bu1);
+        buRepository.save(bu2);
+        buRepository.save(bu3);
+        buRepository.save(bu4);
+        buRepository.save(bu5);
+        buRepository.save(bu6);
+        buRepository.save(bu7);
+        buRepository.save(bu8);
+    }
+
 }
