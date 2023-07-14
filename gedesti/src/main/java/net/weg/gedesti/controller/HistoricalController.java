578f46850ae892a4e8003234553ef5b1c70fee84
Vytor Rosa <86034275+Vytor-Rosa@users.noreply.github.com>
2023-07-14 16:51:50 -0300

    ok

--- gedesti/src/main/java/net/weg/gedesti/controller/HistoricalController.java
+++ gedesti/src/main/java/net/weg/gedesti/controller/HistoricalController.java
@@ -7,6 +7,8 @@ import net.weg.gedesti.model.entity.Historical;
 import net.weg.gedesti.model.service.DemandService;
 import net.weg.gedesti.model.service.HistoricalService;
 import net.weg.gedesti.util.HistoricalUtil;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,48 +17,90 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
-
 
 @RestController
 @AllArgsConstructor
 @RequestMapping("/api/historical")
 public class HistoricalController {
-
-    private HistoricalService historicalService;
     private DemandService demandService;
 
-    @GetMapping
-    public ResponseEntity<List<Historical>> findAll() {
-        return ResponseEntity.status(HttpStatus.FOUND).body(historicalService.findAll());
-    }
+    @PostMapping("/excel")
+    public byte[] generateExcel(@RequestBody List<Demand> demands) throws IOException {
+        try (var workbook = new XSSFWorkbook();
+             var outputStream = new ByteArrayOutputStream()) {
+            CellStyle style = workbook.createCellStyle();
 
-    @PostMapping
-    public ResponseEntity<Object> save(@RequestBody @Valid HistoricalDTO historicalDTO) {
-        Historical historical = new Historical();
-        BeanUtils.copyProperties(historicalDTO, historical);
-        Demand demand = demandService.findById(historical.getDemand().getDemandCode()).get();
-        historical.setNewDemand(demand);
-        return ResponseEntity.status(HttpStatus.CREATED).body(historicalService.save(historical));
-    }
+            Font font = workbook.createFont();
+            font.setBold(true);
+            style.setFont(font);
+            style.setBorderBottom(BorderStyle.THIN);
+            style.setBorderTop(BorderStyle.THIN);
+            style.setBorderRight(BorderStyle.THIN);
+            style.setBorderLeft(BorderStyle.THIN);
+            style.setAlignment(HorizontalAlignment.CENTER);
+            style.setVerticalAlignment(VerticalAlignment.CENTER);
+            style.setWrapText(false);
 
-    @GetMapping("/{historicalCode}")
-    public ResponseEntity<Object> findById(@PathVariable(value = "historicalCode") Integer historicalCode) {
-        Optional<Historical> historicalOptional = historicalService.findById(historicalCode);
-        if (historicalOptional.isEmpty()) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Error! no historical with code: " + historicalCode);
+            CellStyle bodyStyle = workbook.createCellStyle();
+            bodyStyle.setBorderBottom(BorderStyle.THIN);
+            bodyStyle.setBorderTop(BorderStyle.THIN);
+            bodyStyle.setBorderRight(BorderStyle.THIN);
+            bodyStyle.setBorderLeft(BorderStyle.THIN);
+            bodyStyle.setAlignment(HorizontalAlignment.CENTER);
+            bodyStyle.setVerticalAlignment(VerticalAlignment.CENTER);
+            bodyStyle.setWrapText(false);
+
+            var sheet = workbook.createSheet();
+            sheet.setColumnWidth(1, 30 * 256);
+            sheet.setColumnWidth(2, 15 * 256);
+            sheet.setColumnWidth(3, 15 * 256);
+            sheet.setColumnWidth(4, 23 * 256);
+
+            int rowNum = 0;
+            var row = sheet.createRow(rowNum);
+            var cell = row.createCell(0);
+            sheet.autoSizeColumn(0);
+            cell.setCellStyle(style);
+            row = sheet.createRow(rowNum++);
+            cell = row.createCell(0);
+            cell.setCellValue("Usuário");
+            cell.setCellStyle(style);
+            cell = row.createCell(1);
+            cell.setCellValue("Data Alteração");
+            cell.setCellStyle(style);
+            cell = row.createCell(2);
+            cell.setCellValue("Hora Alteração");
+            cell.setCellStyle(style);
+            cell = row.createCell(3);
+            cell.setCellValue("Versão");
+            cell.setCellStyle(style);
+            for (Demand demand : demands) {
+                row = sheet.createRow(rowNum++);
+                cell = row.createCell(0);
+                cell.setCellStyle(bodyStyle);
+                cell.setCellValue(demand.getRequesterRegistration().getWorkerCode());
+                cell = row.createCell(1);
+                cell.setCellStyle(bodyStyle);
+                cell.setCellValue(demand.getDemandDate());
+                cell = row.createCell(2);
+                cell.setCellStyle(bodyStyle);
+                cell.setCellValue(demand.getDemandDate().toString());
+                cell = row.createCell(3);
+                cell.setCellStyle(bodyStyle);
+                cell.setCellValue(demand.getDemandVersion());
+            }
+            workbook.write(outputStream);
+            return outputStream.toByteArray();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return null;
         }
-        return ResponseEntity.status(HttpStatus.FOUND).body(historicalOptional);
+
     }
 
-    @DeleteMapping("/{historicalCode}")
-    public ResponseEntity<Object> deleteById(@PathVariable(value = "historicalCode") Integer historicalCode) {
-        Optional<Historical> historicalOptional = historicalService.findById(historicalCode);
-        if (historicalOptional.isEmpty()) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Error! no historical with code: " + historicalCode);
-        }
-        historicalService.deleteById(historicalCode);
-        return ResponseEntity.status(HttpStatus.FOUND).body(historicalOptional);
-    }
 }
